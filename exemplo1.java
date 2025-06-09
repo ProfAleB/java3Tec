c0444c37004e2f27e6d78d015ac1acf1a9810cb6
Alexandre Boeing <140010324+ProfAleB@users.noreply.github.com>
2025-06-09 19:14:10 +0000

    exemplo1

---
+++ exemplo1.java
@@ -0,0 +1,13 @@
+public class exemplo1 {
+    public static void main(String[] args){
+        String nome = "Alexandre";
+        int idade = 32;
+        double altura = 1.84;
+        boolean estudante = true;
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + "m" );
+        System.out.println("É estudante? " + estudante);
+    }
+}
